6bec7a2341c312384e1145720f00fa8992ffc9a5
przemek <przemek1qaz@gmail.com>
2017-05-26 20:19:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/yebuo_admin/game/View/StartActivity.java
@@ -0,0 +1,20 @@
+package com.example.yebuo_admin.game.View;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.yebuo_admin.game.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_start);
+    }
+}
